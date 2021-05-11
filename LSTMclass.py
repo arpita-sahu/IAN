1fa528c4f29be31eaa930181b05f387d5868ecde
arpita-sahu <67780469+arpita-sahu@users.noreply.github.com>
2021-05-11 00:49:26 -0700

    Update LSTMclass.py

--- LSTMclass.py
+++ LSTMclass.py
@@ -9,13 +9,11 @@ class lstm_model(torch.nn.Module):
     self.num_layers = 1
     self.sig = sig
     self.hidden = n_hidden
-    print("init = ", n_hidden, embedding_dim)
     self.lstm = torch.nn.LSTM(input_size = embedding_dim, hidden_size = n_hidden, batch_first = True)
     self.sigmoid = torch.nn.Sigmoid()
     self.lstm_weight = torch.nn.init.xavier_uniform_(self.lstm.all_weights[0][0]) #weight of the lstm needs to be passed as a parameter to the xavier uniform function
 
   def forward(self, x):
-    print("x = ", x, x.dim(), x.size())
     x = x.view(1, 128, 300) 
     #number of layers? number of layers = number of stacked lstm layers 
     #Input must be 3 dimensional (Sequence len, batch, input dimensions)
@@ -29,11 +27,11 @@ class lstm_model(torch.nn.Module):
     h_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden)) #hidden state
     c_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden))
     output, (h,c) = self.lstm(x, (h_0, c_0))
-    out = h.view(-1, self.hidden_size) #reshaping data 
+    out = h.view(-1, self.hidden) #reshaping data 
 
     #aspect lstm doesn't use the sigmoid function
     if self.sig == True:
-      out = self.sigmoid(hn)
+      out = self.sigmoid(h)
 
     out = self.lstm_weight(out)
     
