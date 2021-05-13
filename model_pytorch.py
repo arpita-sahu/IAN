d578b772391c648a30592c21fb8eff5f4fa9b7d4
arpita-sahu <67780469+arpita-sahu@users.noreply.github.com>
2021-05-13 12:41:18 -0700

    Update model_pytorch.py

--- model_pytorch.py
+++ model_pytorch.py
@@ -51,13 +51,13 @@ class IAN(torch.nn.Module):
         context_outputs = self.context_lstm.forward(context_inputs)
         context_avg = torch.mean(context_outputs, 1)
         
-#         print("\n\n", aspect_outputs.shape, self.aspect_w.shape, torch.unsqueeze(context_avg, -1).shape)
         aspect_outputs = torch.reshape(aspect_outputs, (aspect_outputs.shape[0],aspect_outputs.shape[1], 1))
         aspect_att = torch.nn.functional.softmax(torch.tanh(torch.einsum('ijk,kl,ilm->ijm', aspect_outputs, self.aspect_w,  torch.unsqueeze(context_avg, -1)) + self.aspect_b), dim=1)
         
-        print(type(aspect_att), type(aspect_outputs))
+        #print(type(aspect_att), type(aspect_outputs))
         aspect_rep = torch.sum(aspect_att * aspect_outputs, 1)
-        context_att = torch.nn.Softmax(torch.tanh(torch.einsum('ijk,kl,ilm->ijm', context_outputs, self.context_w, torch.unsqueeze(aspect_avg, -1)) + self.context_b), dim=1)
+        context_outputs = torch.reshape(context_outputs, (context_outputs.shape[0],context_outputs.shape[1], 1))
+        context_att = torch.nn.functional.softmax(torch.tanh(torch.einsum('ijk,kl,ilm->ijm', context_outputs, self.context_w, torch.unsqueeze(aspect_avg, -1)) + self.context_b), dim=1)
         
         context_rep = torch.sum(torch.tensor(context_att) * context_outputs, 1) #find sum along dim 1 
 
