110bdd174d1001b5aaa70d8cac83e7f38bc81d47
arpita-sahu <67780469+arpita-sahu@users.noreply.github.com>
2021-05-19 16:11:57 -0700

    Update model_tf.py

--- model_tf.py
+++ model_tf.py
@@ -57,6 +57,8 @@ class IAN(tf.keras.Model):
         context_rep = tf.reduce_sum(context_att * context_outputs, 1)
 
         rep = tf.concat([aspect_rep, context_rep], 1)
+        print("\n\nREP = ", rep, rep.shape)
         predict = self.output_fc(rep)
+        print("\n\nPREDICT = ",predict, predict.shape) 
 
         return predict, labels
