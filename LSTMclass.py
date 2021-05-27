55a33402bcf1ced8a10a0dfd87bbccf5b423a313
arpita-sahu <67780469+arpita-sahu@users.noreply.github.com>
2021-05-27 13:28:29 -0700

    Update LSTMclass.py

--- LSTMclass.py
+++ LSTMclass.py
@@ -37,15 +37,15 @@ class lstm_model(torch.nn.Module):
     #out = h.view(-1, self.hidden) #reshaping data 
     
     out = output
-    print("h = ", output.shape, h.shape, c.shape)
+    #print("h = ", output.shape, h.shape, c.shape)
     #aspect lstm doesn't use the sigmoid function
     if self.sig == True:
-      print("CONTEXT!!!!!")
+      #print("CONTEXT!!!!!")
       out = self.sigmoid(output)
-      print("sig = ", out.shape)
+      #print("sig = ", out.shape)
     else: 
-      print("ASPECT!!!!")
+      #print("ASPECT!!!!")
 
     out = torch.nn.init.xavier_uniform_(out)#self.lstm_weight(out)
-    print("out = ", out, out.shape)
+    #print("out = ", out, out.shape)
     return out 
