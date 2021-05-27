4171cbf73794ffbf8973c14e9a3c27289f04408e
arpita-sahu <67780469+arpita-sahu@users.noreply.github.com>
2021-05-27 12:36:33 -0700

    Update model_tf.py

--- model_tf.py
+++ model_tf.py
@@ -36,7 +36,7 @@ class IAN(tf.keras.Model):
         aspects, contexts, labels, aspect_lens, context_lens = data
         #print(aspects, contexts, aspect_lens, context_lens)
         aspect_inputs = tf.nn.embedding_lookup(self.embedding_matrix, aspects)
-        print(aspect_inputs.shape)
+        print(aspect_inputs, aspect_inputs.shape)
         aspect_inputs = tf.cast(aspect_inputs, tf.float32)
         print(aspect_inputs.shape)
         aspect_inputs = tf.nn.dropout(aspect_inputs, keep_prob=dropout)
